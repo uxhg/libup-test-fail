e705f60c75ec4ce95e8ac040b9aed74476abc108
Xiuheng <wuxhme@gmail.com>
2021-03-25 14:12:16 +0800

    Update run.sh: extract codeql part into a function
    also add auto naming for codeql related files

--- depgraph/scripts/run.sh
+++ depgraph/scripts/run.sh
@@ -10,6 +10,8 @@ OUT_PATH="${MOD_PATH}/.facts"
 CSLICER_JAR="$HOME/Projects/gitslice/target/cslicer-1.0.0-jar-with-dependencies.jar"
 CSLICER_CFG="${MOD_PATH}/cslicer.properties"
 TOOL_BIN_PATH="../target/debug/"
+DL_PROGRAM_DIR="$HOME/Projects/lib-conflict/libup-test-fail/dl"
+
 
 RED='\033[0;31m'
 NC='\033[0m' # no color
@@ -20,28 +22,41 @@ ERR_CHDIR_FAILED=11
 LOG_DIR="/tmp"
 LOG_QL_DB_C="${LOG_DIR}/ql-db-c.log"
 
+QL_SCRIPT="$HOME/.local/tmp/codeql-repo/java/ql/src/datadp.ql"
+#QL_SCRIPT="$HOME/.local/tmp/codeql-repo/java/ql/src/test.ql"
+_QL_NAME=$(basename "$2")
+QL_NAME=${_QL_NAME%.ql}
+QL_RESULT_CSV="${QL_NAME}.csv"
+
 f_datadp() {
     cd "$MOD_PATH" || exit
-    local QL_SCRIPT="$HOME/.local/tmp/codeql-repo/java/ql/src/datadp.ql"
-    #local QL_SCRIPT="$HOME/.local/tmp/codeql-repo/java/ql/src/test.ql"
-    DB_DIR_NAME="project.db"
-    QL_CSV_NAME="project.csv"
+    local DB_DIR_NAME="project.db"
+    # local QL_CSV_NAME="project.csv"
 
     DB_DIR_PATH=${MOD_PATH:?}/${DB_DIR_NAME}
     if [ -d "$DB_DIR_PATH" ]; then
       echo "Project DB exists, deleting..."
       rm -rf "$DB_DIR_PATH"
       fi
-
-    codeql database create "$DB_DIR_NAME" --language=java --command "mvn clean package -DskipTests" > $LOG_QL_DB_C
-    if [ $? -ne 0 ]; then
-      echo -e "${RED}CodeQL database create failed.${NC}"
-      exit $ERR_MVN_FAILED
-      fi
-
-    codeql query run --database="$DB_DIR_NAME" --output=datadp.bqrs "$QL_SCRIPT"
-    codeql bqrs decode --format=csv -o "$QL_CSV_NAME" datadp.bqrs
+	f_codeql "$DB_DIR_NAME" "$QL_SCRIPT"
     cd "$OLDPWD" || exit $ERR_CHDIR_FAILED
+}
+
+f_codeql() {
+	# $1: directory name of codeql generated database
+	# $2: path to the codeql script
+	echo "$2"
+	codeql database create "$1" --language=java --command \
+		   "mvn clean package -DskipTests" > $LOG_QL_DB_C
+	if [ $? -ne 0 ]; then
+		echo -e "${RED}CodeQL database create failed.${NC}"
+		if [ -d "${MOD_PATH:?}/alt-target" ]; then
+			echo -e "${RED}Consider deleting generated class files in alt-target ${NC}"
+		fi
+		exit $ERR_MVN_FAILED
+	fi
+	codeql query run --database="$1" --output="${QL_NAME}.bqrs" "$2"
+    codeql bqrs decode --format=csv -o "${QL_NAME}.csv" "${QL_NAME}.bqrs"
 }
 
 f_cslicer() {
@@ -62,20 +77,26 @@ fi
 set -x
 
 # pom facts
-${TOOL_BIN_PATH}/pomfact -i "$MOD_PATH" -o "${OUT_PATH}/PomDep.facts" --fmt souffle
+${TOOL_BIN_PATH}/pomfact -i "$MOD_PATH" \
+				-o "${OUT_PATH}/PomDep.facts" \
+				--fmt souffle
 
 # run codeql program for data flow
 f_datadp
 
 # generate facts from codeql results
-${TOOL_BIN_PATH}/dpfact -i "${MOD_PATH}/${QL_CSV_NAME}" --ex "java."  --ex "<anonymous class>" -o "${OUT_PATH}/DataFlowVMethod.facts" --fmt souffle
+${TOOL_BIN_PATH}/dpfact -i "${MOD_PATH}/${QL_RESULT_CSV}"\
+				--ex "java."  --ex "<anonymous class>" \
+				-o "${OUT_PATH}/DataFlowVMethod.facts" \
+				--fmt souffle
 
 # JAR-contain-Class facts and gen config for CSlicer
-${TOOL_BIN_PATH}/clsfact -i "$MOD_PATH" --cslicer -o "${OUT_PATH}/ContainClass.facts"
+${TOOL_BIN_PATH}/clsfact -i "$MOD_PATH" --cslicer \
+    -o "${OUT_PATH}/ContainClass.facts"
 
 # invoke CSlicer
 f_cslicer
 
 # if multiple module: need to move CSlicer generated facts into mod_path/.facts
 # invoke souffle
-#souffle-orig -F "$MOD_PATH/.facts"  '../dl/def.dl' -D '../dl/output'
+souffle-orig -F "$MOD_PATH/.facts"  "${DL_PROGRAM_DIR}/def.dl" -D "${DL_PROGRAM_DIR}/output"
