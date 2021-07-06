144b4ac3269310ab8f85718d2502082c28cb47ea
Xiuheng <wuxhme@gmail.com>
2021-07-07 02:51:44 +0800

    Implement read/write stat_file

--- depgraph/src/bin/make_local_repo_store.rs
+++ depgraph/src/bin/make_local_repo_store.rs
@@ -6,17 +6,51 @@ use git2::Repository;
 
 use log::{error,info,warn};
 use depgraph::utils::existing_data_utils::RepoAtVer;
+use depgraph::utils::utils;
 use url::Url;
+use std::collections::HashMap;
+use std::io::BufWriter;
+use std::fs::File;
 
 fn main() {
     let matches = handle_args();
     let project_list_file = matches.value_of("Input").unwrap();
     let repo_storage_loc = matches.value_of("Storage").unwrap();
     let stat_file = Path::new(matches.value_of("Stat").unwrap());
+
+    // read cloned repos
+    let mut existing_stat = read_existing_stat(stat_file.as_ref());
+
     let projects = RepoAtVer::batch_create_from_json(project_list_file);
     for x in projects {
+        let repo_url = x.url();
+        if existing_stat.contains_key(repo_url) {
+            info!("Skip {}, already exists @ {}", repo_url, existing_stat.get(repo_url)
+                .unwrap_or(&String::from("NOT_FOUND")));
+            continue
+        }
         info!("Cloning on {:?}", x);
-        clone_remote(x.url(), Path::new(repo_storage_loc), stat+);
+        if let Some(r) = clone_remote(x.url(), Path::new(repo_storage_loc), stat_file) {
+            existing_stat.insert(repo_url.to_string(), String::from(r.path().to_str().unwrap_or_default()));
+        }
+    }
+    // write back existing_stat to file
+    let stat_writer = BufWriter::new(File::create(stat_file)
+        .expect("Cannot open or create stat file"));
+    serde_json::ser::to_writer_pretty(stat_writer, &existing_stat);
+}
+
+fn read_existing_stat(stat_file: &Path) -> HashMap<String, String> {
+    match stat_file.exists() {
+        true => {
+            let stat_reader = utils::load_json(stat_file);
+            serde_json::from_reader::<_, HashMap<String, String>>(stat_reader)
+                .expect("Stat file should be a hashmap between repo_url and local location")
+        },
+        false => {
+            info!("{} does not exist, will create new", stat_file.to_str().unwrap_or_default());
+            HashMap::new()
+        }
     }
 }
 
@@ -37,9 +71,9 @@ fn clone_remote(url: &str, local_path: &Path, stat_file: &Path) -> Option<Reposi
             }
         }
     };
+
     match Repository::clone(url, clone_to_path) {
         Ok(repo) => {
-
             Some(repo)
         },
         Err(e) => {
