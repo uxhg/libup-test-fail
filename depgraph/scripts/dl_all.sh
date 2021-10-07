d2b777a6d5dcbc6fb909de9b2f4bf4a373d16e33
Xiuheng <wuxhme@gmail.com>
2021-10-07 14:15:54 +0800

    Small

--- depgraph/scripts/dl_all.sh
+++ depgraph/scripts/dl_all.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-for f in ~/Projects/apictx/python-tools/each-proj/*.list ;
+# for f in ~/Projects/apictx/python-tools/each-proj/*.list ;
+for f in ~/Projects/apictx/python-tools/each-proj--cli-usage-at-least-500/*.list ;
 do
     _BASE_NAME=${f##*/}
     BASE_NAME=${_BASE_NAME%.list}
     echo "$BASE_NAME";
     RUST_LOG=info ../target/debug/get_jar_remote -l "$f" \
-        --dir "${HOME}"/.local/share/apictx-subjects/jars/for-"${BASE_NAME}" \
+        --dir "${HOME}"/.local/share/apictx-subjects/jars/limit-500/for-"${BASE_NAME}" \
         --storage ~/.local/share/apictx-subjects/jars/jars-storage/
 done
