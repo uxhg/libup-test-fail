25178f6e21816b8492d48d6f80b6cb228509a459
Xiuheng <wuxhme@gmail.com>
2021-10-13 22:24:19 +0800

    Add the script for removing 404 "jar"s

---
+++ depgraph/scripts/rm404.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# a simple script to remove 404 "jar"s wrongly downloaded before fixing the download tool
+# used together with find . -type f -iname "*.jar" -exec /path/to/rm404.sh {} \;
+# need to make sure the relative paths (i.e., the string in $1) do not contain semicolon
+# otherwise, need to modify the cut command
+set -eu
+
+if [[ $(file "$1" | cut -d: -f2) ==  " HTML document, ASCII text" ]]; then 
+    rm "$1"; 
+else
+    echo "not 404";
+fi
