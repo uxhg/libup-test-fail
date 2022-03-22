3ea6c1b55ef80d563cf6f842ade0827b026386bc
Xiuheng <wuxhme@gmail.com>
2022-03-23 05:56:00 +0800

    Improve find_usage_instance

--- depgraph/scripts/find_usage_instance.sh
+++ depgraph/scripts/find_usage_instance.sh
@@ -5,7 +5,12 @@ JAR_STORE="$HOME/data/jars-storage"
 MVN_COORD=$1
 FILE_NAME=$2
 LINE_NUM=$3
+LINE_MAX=$4
 
 DIRNAME=${MVN_COORD//:/--}
-bat -r ${LINE_NUM}:$((LINE_NUM+20)) $(find ${JAR_STORE}/${DIRNAME}/unpack -iname ${FILE_NAME}.java)
+FOUND_FILE=$(find ${JAR_STORE}/${DIRNAME}/unpack -iname ${FILE_NAME}.java)
+echo $FOUND_FILE
 
+if [[ -n $FOUND_FILE ]] ; then 
+bat -r ${LINE_NUM}:$((LINE_NUM+${LINE_MAX:-20}))  $FOUND_FILE;
+fi
