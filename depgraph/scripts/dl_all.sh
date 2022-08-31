f038120e8ed9132a5ac36409b3b580f915f53340
Xiuheng <wuxhme@gmail.com>
2022-08-31 21:27:34 +0800

    Quote variable to prevent wordsplitting

--- depgraph/scripts/dl_all.sh
+++ depgraph/scripts/dl_all.sh
@@ -5,7 +5,7 @@ LINK_BASE_DIR=$2
 # in old experiments, this dir is  "${HOME}"/data/limit-100/ on server
 #   or ~/.local/share/apictx-subjects/jars/limit-500 on local
 
-for f in ${LIST_DIR}/*.list; # do not quote, let * expand
+for f in "$LIST_DIR"/*.list;
 # for f in ~/data/each-proj/*.list ; # server
 # for f in ~/Projects/apictx/python-tools/each-proj--cli-usage-at-least-500/*.list ; # local 
 do
